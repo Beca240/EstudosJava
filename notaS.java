bc0cf5e052d13cd969405e8e5004e6e8eb93c7bc
Rebeca Suzuki <rebecamoreira1824@gmail.com>
2025-10-10 09:44:15 -0300

    Primeiro commit

---
+++ notaS.java
@@ -0,0 +1,23 @@
+public class notaS{
+	public static void main(String[]args){
+
+	int nota1 = 7;  
+	int nota2 = 5;
+	
+	double media = (nota1 + nota2) / 2;
+
+	if (media>=6){
+
+	System.out.println( "Aprovado!");
+
+	}
+	else{
+
+	System.out.println("Reprovado!");
+	}
+
+
+
+
+	}
+}
---
+++ notasFrequencia.java
@@ -0,0 +1,21 @@
+public class notasFrequencia{
+	public static void main(String[]args){
+
+	int nota1 = 8;  
+	int nota2 = 6;
+	int frequencia =80;
+
+	double media = (nota1 + nota2) / 2;
+
+	if (media>=7 && frequencia>=75){
+
+	System.out.println( "Aprovado!");
+
+	}
+	else{
+
+	System.out.println("Reprovado!");
+		}
+
+	}
+}
